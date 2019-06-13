13b342c8449bf87105029134c3a347c4396886be
Luukas <luukki123@gmail.com>
2019-06-13 15:05:51 +0300

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,61 @@
+var express = require('express');
+require('dotenv').config();
+var http = require('http');
+var https = require('https');
+var router = express.Router();
+var SpotifyWebApi = require('spotify-web-api-node');
+
+var app = require('../app');
+var longpoll = require('express-longpoll') (app, {
+  DEBUG : true
+});
+
+var scopes = ['user-read-private', 'user-read-email'];
+
+var spotifyApi = new SpotifyWebApi({
+  clientId: process.env.clientId,
+  clientSecret: process.env.clientSecret,
+  redirectUri: 'http://localhost:3000/redirect'
+});
+
+var state;
+
+longpoll.create('/poll/:id', (req,res,next) => {
+  req.id = req.params.id;
+  next();
+});
+
+router.get('/me', function(req,res,next){
+  //https.get('https://accounts.spotify.com/authorize?client_id=86dab2ec88ef49cb98f1f308a7eecb7a&response_type=token&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Flistk&scope=user-read-private', (res)=>{
+    //console.log(res.status);
+  //})
+  state = req.query.state;
+  var authorizeurl = spotifyApi.createAuthorizeURL(scopes, state);
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8100')
+  res.json({"url": authorizeurl});
+
+
+})
+
+router.get('/redirect', (req,res,next) => {
+  var code = req.query.code; 
+  spotifyApi.authorizationCodeGrant(code).then(
+    (data) => {
+      console.log('The token expires in ' + data.body['expires_in']);
+      console.log('The access token is ' + data.body['access_token']);
+      console.log('The refresh token is ' + data.body['refresh_token']); 
+      
+      spotifyApi.setAccessToken(data.body['access_token']);
+      spotifyApi.setRefreshToken(data.body['refresh_token']);
+      
+
+      longpoll.publishToId("poll/:id", state, data);
+
+      res.status(200).send();
+    },
+    (err) => {
+      console.log(err);
+    }
+  )
+})
+module.exports = router;
